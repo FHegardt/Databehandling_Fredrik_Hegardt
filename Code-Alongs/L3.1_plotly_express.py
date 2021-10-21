346009e0d9956db60a9070da1a47bfaf66863490
FHegardt <39218412+FHegardt@users.noreply.github.com>
2021-10-21 10:42:05 +0200

    woho

---
+++ Code-Alongs/L3.1_plotly_express.py
@@ -0,0 +1,13 @@
+from _plotly_utils.basevalidators import type_str
+import plotly_express as px
+
+gapminder = px.data.gapminder()
+
+nordic = gapminder[gapminder["country"].isin(["Sweden", "Denmark", "Norway", "Iceland", "Finland"])]
+test = gapminder[gapminder["continent"].isin(["Americas"])]
+
+fig = px.scatter(test, x = "gdpPercap", y = "lifeExp", 
+    size = "pop", color = "country", size_max = 70,
+    log_x = True, animation_frame = "year", title="Gapminder",
+    range_x = [100, 100000], range_y = [25,90])
+fig.show()
