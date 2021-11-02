8932cb300ddccef90952930cf63b2da1085918ad
FHegardt <39218412+FHegardt@users.noreply.github.com>
2021-11-02 10:53:22 +0100

    working!

--- Code-Alongs/L6.stock_dashboard/L6.2_dashboard/load_data1.py
+++ Code-Alongs/L6.stock_dashboard/L6.2_dashboard/load_data1.py
@@ -5,14 +5,14 @@ print(df.head())
 
 class Stockdatalocal:
     """Class to get local stock data"""
-    def __init__(self, data_folder_path: str = "../data/") -> None:
+    def __init__(self, data_folder_path: str = "data/") -> None:
         self.data_folder_path = data_folder_path
     
     def stock_dataframe(self, stockname: str) -> list:
         """Return list of two dataframes, one for daily time series."""
 
         stock_df_list = []
-        for path_ending in ["_TIME_SERIES_DAILY.csv","TIME_SERIES_INTRADAY_EXTENDED.csv"]:
+        for path_ending in ["_TIME_SERIES_DAILY.csv","_TIME_SERIES_INTRADAY_EXTENDED.csv"]:
             path = self.data_folder_path+stockname+path_ending
             stock = pd.read_csv(path, index_col=0,parse_dates=True)
             stock.index.rename("Date", inplace = True)
--- Code-Alongs/L6.stock_dashboard/L6.2_dashboard/main1.py
+++ Code-Alongs/L6.stock_dashboard/L6.2_dashboard/main1.py
@@ -1,3 +1,4 @@
+from dash.dcc.Slider import Slider
 import pandas as pd
 from dash import dcc, html
 import dash
@@ -7,12 +8,14 @@ import plotly_express as px
 
 stock_data_object = Stockdatalocal()
 
-symbol_dict = dict(AAPL = "Apple", TSLA = "Tesla", NVIDIA = "Nividia")
+symbol_dict = dict(AAPL = "Apple", TSLA = "Tesla", NVDA = "Nividia")
 
 stock_options_dropdown = [{"label": name, "value": symbol}
                          for symbol, name in symbol_dict.items()]
 df_dict = {symbol: stock_data_object.stock_dataframe(symbol)
             for symbol in symbol_dict}
+
+slider_marks = {i : mark for i,mark in enumerate(["1 day", "1 week", "1 month", "3 month", "1 year", "5 year", "Max"])}
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
@@ -21,19 +24,23 @@ app.layout = html.Div([
     dcc.Dropdown(id='stock-picker-dropdown', className='',options = stock_options_dropdown,
     value = 'AAPL'
     ),
-    dcc.Graph(id='stock-graph', classname = '')
- 
+    dcc.Graph(id='stock-graph', className = ''),
+    dcc.Slider(id='time-slider', className = '',
+    min = 0, max = 6, step = None, value = 2, marks = slider_marks)
     ])
 
 @app.callback(
     Output("stock-graph","figure"),
-    Input("stock-picker-dropdown", "value")
+    Input("stock-picker-dropdown", "value"),
+    Input("time-slider", "value")
 )
-def update_graph(stock):
+def update_graph(stock, time_index):
 
     dff_daily , dff_intraday = df_dict[stock]
 
-    fig = px.line(dff_daily, x = dff.index, y = "close")
+    dff = dff_intraday if time_index <= 2 else dff_daily
+
+    fig = px.line(dff, x = dff.index, y = "close")
 
     return fig #fig object goes into output property i.e figure property
 
