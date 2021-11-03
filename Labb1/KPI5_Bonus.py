20a48067d07445826053c435003ab6ec64084dc6
FHegardt <39218412+FHegardt@users.noreply.github.com>
2021-11-03 14:53:33 +0100

    ADded KPI 5

--- Labb1/KPI5_Bonus.py
+++ Labb1/KPI5_Bonus.py
@@ -6,8 +6,9 @@ import plotly_express as px
 vaccin = pd.ExcelFile('Folkhalsomyndigheten_Covid19_Vaccine.xlsx')
 vaccin_kommun = pd.read_excel(vaccin, 'Vaccinerade tidsserie')
 
-fig = px.scatter(vaccin_kommun, x = "Vecka", y = "Andel vaccinerade", 
+fig = px.scatter(vaccin_kommun, x = "Vecka", y = "Andel vaccinerade"[0], 
+    size= "Antal vaccinerade",
     color = "Region", size_max = 70,
     log_x = False, animation_frame = "Vecka", title="Gapminder",
-    range_x = [100, 100000], range_y = [25,90])
+    range_x = [0, 45], range_y = [0,1])
 fig.show()
