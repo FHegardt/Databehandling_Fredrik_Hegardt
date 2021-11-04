f7aadc13da2bf6306807396425dfe418072ef245
FHegardt <39218412+FHegardt@users.noreply.github.com>
2021-11-04 11:49:44 +0100

    ok

--- Labb1/KPI5bonus.py
+++ Labb1/KPI5bonus.py
@@ -16,14 +16,14 @@ vaccin = pd.ExcelFile("Folkhalsomyndigheten_Covid19_Vaccine.xlsx")
 vaccin_kommun3 = pd.read_excel(vaccin, 'Vaccinerade tidsserie')
 vaccin_kommun4 = pd.read_excel(vaccin, 'Vaccinerade tidsserie')
 
+# droppar dubletter så att Varje region bara finns med på ett ställe.
 vaccin_kommun3 = vaccin_kommun3.drop_duplicates(subset = ("Region", "Vecka"))
-
 vaccin_kommun4 = vaccin_kommun4.drop_duplicates(subset = ("Region","Vecka"), keep= "last")
 
-
+# Mergar på "Inner" på Vecka och Region för att få ut den datan jag behöver.
 bigdata = vaccin_kommun3.merge(vaccin_kommun4, how= "inner", on = ("Vecka", "Region"))
 bigdata = bigdata.rename({"Andel vaccinerade_x" : "Vaccinerade med en dos", "Andel vaccinerade_y" : "Fullvaccinerade"}, axis = 1)
-
+# Skapar själva plotten i plotly, som ett rörligt diagram.
 fig = px.scatter(bigdata, x =  "Vaccinerade med en dos" , y = "Fullvaccinerade", 
     size= "Antal vaccinerade_x",
     title = " Andelen vaccinerade med en / två doser per kommun och vecka från 2020-v55 till 2021-v41",
